e3e784518a6c3bdc6678c390ad2a02c860413969
Chad Freer <chad.freer@cern.ch>
2018-02-20 13:18:26 +0100

    Add new files 	new file:   EMTFAnalyzer/NTupleMaker/interface/FlatNtupleBranches/CSCSegInfo.h 	new file:   EMTFAnalyzer/NTupleMaker/src/FlatNtupleBranches/.CSCSegInfo.cc.swo 	new file:   EMTFAnalyzer/NTupleMaker/src/FlatNtupleBranches/CSCSegInfo.cc

---
+++ EMTFAnalyzer/NTupleMaker/interface/FlatNtupleBranches/CSCSegInfo.h
@@ -0,0 +1,34 @@
+
+#ifndef FlatNtupleBranchesCSCSegInfo_h
+#define FlatNtupleBranchesCSCSegInfo_h
+
+// Common branch info
+#include "EMTFAnalyzer/NTupleMaker/interface/FlatNtupleBranches/Common.h"
+
+// Helpful tools
+#include "EMTFAnalyzer/NTupleMaker/interface/HelperFunctions.h"
+
+// CSC segment classes
+#include "DataFormats/CSCRecHit/interface/CSCSegmentCollection.h"
+
+////////////////////////////////
+///  EMTF track information  ///
+////////////////////////////////
+struct CSCSegInfo {
+  std::vector<TString> ints = {{"nSegs"}};
+  //std::vector<TString> vFlt = {{"seg_eta"}};
+  std::vector<TString> vFlt = {{"seg_chi2", "seg_time", "seg_segx", "seg_segy", "seg_segDirx", "seg_segDiry", "seg_Dirz"}};
+  std::vector<TString> vInt = {{"seg_endcap", "seg_ring", "seg_station", "seg_chamber", "seg_nRecHits"}};
+  std::map<TString, int> mInts;
+  std::map<TString, std::vector<float> > mVFlt;
+  std::map<TString, std::vector<int> > mVInt;
+
+  void Initialize();
+  void Reset();
+  //void Fill(const edm::Handle<CSCSegmentCollection> & cscSeg, std::vector<CSCRecHit2D> theCSCRecHits);
+  //void Fill(const edm::OwnVector<CSCSegment> & cscSeg);
+  void Fill(const edm::Handle<CSCSegmentCollection> & cscSeg);
+};
+
+
+#endif  // #ifndef FlatNtupleBranchesCSCSegInfo_h
---
+++ EMTFAnalyzer/NTupleMaker/src/FlatNtupleBranches/CSCSegInfo.cc
@@ -0,0 +1,41 @@
+
+#include "EMTFAnalyzer/NTupleMaker/interface/FlatNtupleBranches/CSCSegInfo.h"
+
+void CSCSegInfo::Initialize() {
+  for (auto & str : ints)  mInts .insert( std::pair<TString, int>(str, DINT) );
+  for (auto & str : vFlt)  mVFlt .insert( std::pair<TString, std::vector<float> >(str, DVFLT) );
+  for (auto & str : vInt)  mVInt .insert( std::pair<TString, std::vector<int> >  (str, DVINT) );
+}
+
+void CSCSegInfo::Reset() {
+  for (auto & it : mInts)  it.second = DINT;
+  for (auto & it : mVFlt)  it.second.clear();
+  for (auto & it : mVInt)  it.second.clear();
+  INSERT(mInts, "nSegs", 0);
+ // INSERT(mInts, "nSegsBX0", 0);
+}
+
+//void CSCSegInfo::Fill(const edm::Handle<CSCSegmentCollection> & cscSeg, std::vector<CSCRecHit2D> theCSCRecHits) {
+void CSCSegInfo::Fill(const edm::Handle<CSCSegmentCollection> & cscSeg) {
+//std::vector<CSCRecHit2D> theCSCRecHits
+  // std::cout << "Filling CSCSegInfo" << std::endl;
+
+  INSERT(mInts, "nSegs", ACCESS(mInts, "nSegs") + 1 );
+  //if (cscSeg.tbins() == 8) 
+    //INSERT(mInts, "nSegsBX0", ACCESS(mInts, "nSegsBX0") + 1 );
+
+  INSERT(mVFlt, "seg_chi2",          CSCSegment().chi2() );
+  INSERT(mVFlt, "seg_time",          CSCSegment().time() );
+  INSERT(mVFlt, "seg_segx",          CSCSegment().localPosition().x() );
+  INSERT(mVFlt, "seg_segy",          CSCSegment().localPosition().y() );
+  INSERT(mVFlt, "seg_segDirx",       CSCSegment().localDirection().x() );
+  INSERT(mVFlt, "seg_segDiry",       CSCSegment().localDirection().y() );
+  INSERT(mVFlt, "seg_segDirz",       CSCSegment().localDirection().z() );
+
+  INSERT(mVInt, "seg_endcap",        CSCDetId().endcap() );
+  INSERT(mVInt, "seg_ring",	     CSCDetId().ring() );
+  INSERT(mVInt, "seg_station",       CSCDetId().station() );
+  INSERT(mVInt, "seg_chamber",       CSCDetId().chamber() );
+  INSERT(mVInt, "seg_nRecHits",      CSCSegment().nRecHits() );
+  // std::cout << "Filled CSCSegInfo" << std::endl;
+} // End function: CSCSegInfo::Fill(const l1t::CSCSeg & cscSeg)
